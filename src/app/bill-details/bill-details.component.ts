a17ff583c5fc33877132d9b6b25191e8166331e2
darthionic <ranjith.varatharajan@icloud.com>
2017-05-10 11:28:22 +0530

    Final

--- src/app/bill-details/bill-details.component.ts
+++ src/app/bill-details/bill-details.component.ts
@@ -1,17 +1,13 @@
 import { Component, OnInit,Input } from '@angular/core';
 import { BillModel } from '../../models/billmodel'
-
 @Component({
   selector: 'app-bill-details',
   templateUrl: './bill-details.component.html',
   styleUrls: ['./bill-details.component.css']
 })
 export class BillDetailsComponent implements OnInit {
-
   @Input() bill: BillModel
   constructor() { }
-
   ngOnInit() {
   }
-
 }
--- src/app/bill-list/bill-list.component.ts
+++ src/app/bill-list/bill-list.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { BillModel } from '../../models/billmodel'
-
 @Component({
   selector: 'app-bill-list',
   templateUrl: './bill-list.component.html',
   styleUrls: ['./bill-list.component.css']
 })
 export class BillListComponent implements OnInit {
-
   @Input() billList: BillModel
   @Output() selectedBill: EventEmitter<BillModel> = new EventEmitter<BillModel>()
   constructor() { }
@@ -16,5 +14,4 @@ export class BillListComponent implements OnInit {
   }
   ngOnInit() {
   }
-
 }
